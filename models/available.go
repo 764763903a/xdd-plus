d613b824a29d15507ad62ee8e54d3c6a5cfe7f26
cdle <798731886@qq.com>
2021-08-18 23:04:35 +0800

    x

--- models/available.go
+++ models/available.go
@@ -133,7 +133,7 @@ func initCookie() {
 }
 
 func CookieOK(ck *JdCookie) bool {
-	// Egg(fmt.Sprintf("pt_key=%s;pt_pin=%s;", ck.PtKey, ck.PtPin))
+	// tytCoupon(fmt.Sprintf("pt_key=%s;pt_pin=%s;", ck.PtKey, ck.PtPin))
 	if ck == nil {
 		return true
 	}
