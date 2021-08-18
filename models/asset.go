6211d13eccbaf90d5b2b9111b37af99804ab7a90
cdle <798731886@qq.com>
2021-08-18 23:19:38 +0800

    x

--- models/asset.go
+++ models/asset.go
@@ -169,7 +169,7 @@ func (ck *JdCookie) Query() string {
 				return ""
 			}
 			msgs = append(msgs, []string{
-				fmt.Sprintf("所有红包：%.2f%s元", asset.RedPacket.Total, e(asset.RedPacket.ToExpire)),
+				fmt.Sprintf("所有红包：%.2f%s元🧧", asset.RedPacket.Total, e(asset.RedPacket.ToExpire)),
 				fmt.Sprintf("京喜红包：%.2f%s元", asset.RedPacket.Jx, e(asset.RedPacket.ToExpireJx)),
 				fmt.Sprintf("极速红包：%.2f%s元", asset.RedPacket.Js, e(asset.RedPacket.ToExpireJs)),
 				fmt.Sprintf("健康红包：%.2f%s元", asset.RedPacket.Jk, e(asset.RedPacket.ToExpireJk)),
