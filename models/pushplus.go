db0cf07f2668ef603cb8e8ce999856e8131f2b67
cdle <798731886@qq.com>
2021-08-19 11:17:37 +0800

    x

--- models/pushplus.go
+++ models/pushplus.go
@@ -10,27 +10,21 @@ func pushPlus(token string, content string) {
 	if token == "" {
 		return
 	}
+	data, _ := json.Marshal(struct {
+		Token    string `json:"token"`
+		Content  string `json:"content"`
+		Template string `json:"template"`
+	}{
+		Token:    token,
+		Content:  content,
+		Template: "txt",
+	})
 	req := httplib.Post("http://pushplus.hxtrip.com/send")
 	req.Header("Content-Type", "application/json")
-	data, _ := json.Marshal(struct {
-		Token   string `json:"token"`
-		Content string `json:"Content"`
-	}{
-		Token:   token,
-		Content: content,
-	})
 	req.Body(data)
 	req.Response()
-
 	req = httplib.Post("http://www.pushplus.plus/send")
 	req.Header("Content-Type", "application/json")
-	data, _ = json.Marshal(struct {
-		Token   string `json:"token"`
-		Content string `json:"Content"`
-	}{
-		Token:   token,
-		Content: content,
-	})
 	req.Body(data)
 	req.Response()
 }
