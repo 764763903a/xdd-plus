16919e23c44b9fc8b1b570606788957f851ae2e8
cdle <798731886@qq.com>
2021-08-20 16:47:47 +0800

    x

--- models/admin.go
+++ models/admin.go
@@ -225,5 +225,5 @@ func Count() string {
 			tl++
 		}
 	}
-	return fmt.Sprintf("总数%d,有效%d,无效%d,今日失效%d,今日扫码%d,今日新增%d", zs, yx, wx, tc, ts, tl)
+	return fmt.Sprintf("总数%d,有效%d,无效%d,今日失效%d,今日扫码%d,今日新增%d", zs, yx, wx, tl, ts, tc)
 }
