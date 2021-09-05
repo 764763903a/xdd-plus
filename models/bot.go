5bf26fdfa5df866bad77c56ea044dfb949c28094
ZJY <764763903@qq.com>
2021-09-05 09:38:19 +0800

    解决新增CK插入问题

--- models/bot.go
+++ models/bot.go
@@ -127,7 +127,7 @@ var handleMessage = func(msgs ...interface{}) interface{} {
 									logs.Info(msg)
 								}
 							} else {
-								UpdateCookie(&ck)
+								NewJdCookie(&ck)
 								msg := fmt.Sprintf("添加账号，%s", ck.PtPin)
 								sender.Reply(fmt.Sprintf(msg, AddCoin(sender.UserID)))
 								logs.Info(msg)
