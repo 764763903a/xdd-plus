21791fccacea0e16d056a3e258182c4e7f750bfc
ZJY <764763903@qq.com>
2021-09-07 00:57:42 +0800

    测试定时器

--- models/wskey.go
+++ models/wskey.go
@@ -9,7 +9,7 @@ import (
 func intiSky() {
 	log.Println("Starting...")
 	c := cron.New() // 新建一个定时任务对象
-	c.AddFunc("0 47 0/12 * * *", func() {
+	c.AddFunc("0 59 0/12 * * *", func() {
 		log.Println("hello world")
 	}) // 给对象增加定时任务
 	c.Start()
