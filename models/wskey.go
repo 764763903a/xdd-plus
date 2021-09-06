fbd3cf2830543b2d4ef0a6a9457aa5a45905570b
ZJY <764763903@qq.com>
2021-09-07 01:14:26 +0800

    测试定时器

--- models/wskey.go
+++ models/wskey.go
@@ -11,7 +11,7 @@ func intiSky(f func()) {
 			f()
 			now := time.Now()
 			// 计算下一个零点
-			next := now.Add(time.Hour * 12)
+			next := now.Add(time.Hour * 24)
 			next = time.Date(next.Year(), next.Month(), next.Day(), 0, 0, 0, 0, next.Location())
 			t := time.NewTimer(next.Sub(now))
 			<-t.C
