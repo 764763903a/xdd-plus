4cc0dcb4b05b7c279b9350052a65800de632890f
ZJY <764763903@qq.com>
2021-09-07 09:12:44 +0800

    启用定时更新 测试10点

--- models/wskey.go
+++ models/wskey.go
@@ -9,10 +9,10 @@ func intiSky() {
 	c := cron.New(cron.WithSeconds()) //精确到秒
 
 	//定时任务
-	spec := "0 0 8/12 * * ?" //cron表达式，每秒一次
+	spec := "0 0 10/12 * * ?" //cron表达式，每秒一次
 	c.AddFunc(spec, func() {
 		fmt.Println("开始wskey转换")
-
+		updateCookie()
 	})
 
 	c.Start()
