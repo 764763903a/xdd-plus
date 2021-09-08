208928d028c2df6145208074cd70c51bf9a12858
ZJY <764763903@qq.com>
2021-09-08 11:15:47 +0800

    修复更新无提示

--- models/version.go
+++ models/version.go
@@ -11,7 +11,7 @@ import (
 	"github.com/beego/beego/v2/core/logs"
 )
 
-var version = "20210907"
+var version = "20210908"
 var describe = "定时更新"
 var AppName = "xdd"
 var pname = regexp.MustCompile(`/([^/\s]+)`).FindStringSubmatch(os.Args[0])[1]
