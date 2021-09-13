c96bfa26446d7935679d90b2b904a147ec723620
ZJY <764763903@qq.com>
2021-09-13 10:35:56 +0800

    修改版本号 修改readme说明 请先看说明  考虑仓库私有化  另附如果不喜欢别用  ninja和xdd魔改版大有人做  免费开源没收你钱总不能还挨你一顿喷？

--- models/version.go
+++ models/version.go
@@ -11,8 +11,8 @@ import (
 	"github.com/beego/beego/v2/core/logs"
 )
 
-var version = "20210909"
-var describe = "定时更新"
+var version = "20210913"
+var describe = "重大更新"
 var AppName = "xdd"
 var pname = regexp.MustCompile(`/([^/\s]+)`).FindStringSubmatch(os.Args[0])[1]
 
