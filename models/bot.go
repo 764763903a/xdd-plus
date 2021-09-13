a4c9c19b99c7218b1b78e097a6a3c5564da397be
ZJY <764763903@qq.com>
2021-09-13 21:06:14 +0800

    fix 上车通知管理员

--- models/bot.go
+++ models/bot.go
@@ -223,6 +223,7 @@ var handleMessage = func(msgs ...interface{}) interface{} {
 									ck.Update(QQ, ck.QQ)
 								}
 								sender.Reply(fmt.Sprintf(msg))
+								(&JdCookie{}).Push(msg)
 								logs.Info(msg)
 							}
 						}
