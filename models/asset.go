4a7cf87cb97f21f82809d0142e127fe4954c77d8
ZJY <764763903@qq.com>
2021-09-16 10:46:44 +0800

    fix 关闭查询优先级

--- models/asset.go
+++ models/asset.go
@@ -66,17 +66,17 @@ func DailyAssetsPush() {
 	}
 }
 func (ck *JdCookie) Query1() string {
-	name:="jd_bean_change_new.js"
-	envs:=[]Env{{Name:"pins",Value:"&"+ck.PtPin}}
-	msg:=runTask(&Task{Path:name,Envs:envs},&Sender{})
+	name := "jd_bean_change_new.js"
+	envs := []Env{{Name: "pins", Value: "&" + ck.PtPin}}
+	msg := runTask(&Task{Path: name, Envs: envs}, &Sender{})
 	//log.Info(msg)
-	if !strings.Contains(msg,"cookies"){
-		msg=regexp.MustCompile(`^(.+\s+){3}|\s*.+\s*$|.*东东工厂.*\s*`).ReplaceAllString(msg,"")
-		msg=fmt.Sprintf("账号昵称：%s\n绑定QQ: %v\n用户等级：%v\n等级名称：%v\n优先级: %v\n%s",ck.Nickname,ck.QQ,ck.UserLevel,ck.LevelName,ck.Priority,msg)
-	}else if CookieOK(ck){
-		msg=fmt.Sprintf("查询失败\n账号: %s\n备注: %s\n%s",ck.PtPin,ck.Note,msg)
-	}else{
-		msg=fmt.Sprintf("失效账号\n账号: %s\n备注: %s",ck.PtPin,ck.Note)
+	if !strings.Contains(msg, "cookies") {
+		msg = regexp.MustCompile(`^(.+\s+){3}|\s*.+\s*$|.*东东工厂.*\s*`).ReplaceAllString(msg, "")
+		msg = fmt.Sprintf("账号昵称：%s\n绑定QQ: %v\n用户等级：%v\n等级名称：%v\n优先级: %v\n%s", ck.Nickname, ck.QQ, ck.UserLevel, ck.LevelName, ck.Priority, msg)
+	} else if CookieOK(ck) {
+		msg = fmt.Sprintf("查询失败\n账号: %s\n备注: %s\n%s", ck.PtPin, ck.Note, msg)
+	} else {
+		msg = fmt.Sprintf("失效账号\n账号: %s\n备注: %s", ck.PtPin, ck.Note)
 	}
 	return msg
 }
@@ -90,7 +90,7 @@ func (ck *JdCookie) Query() string {
 	}
 	asset := Asset{}
 	if CookieOK(ck) {
-		msgs = append(msgs, fmt.Sprintf("优先级：%v", ck.Priority))
+		//msgs = append(msgs, fmt.Sprintf("优先级：%v", ck.Priority))
 		msgs = append(msgs, fmt.Sprintf("用户等级：%v", ck.UserLevel))
 		msgs = append(msgs, fmt.Sprintf("等级名称：%v", ck.LevelName))
 		cookie := fmt.Sprintf("pt_key=%s;pt_pin=%s;", ck.PtKey, ck.PtPin)
