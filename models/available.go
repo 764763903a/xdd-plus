5a850061b0dd41606d87c7d8b208445d9596d34a
ZJY <764763903@qq.com>
2021-09-19 10:43:24 +0800

    fix 多次失效

--- models/available.go
+++ models/available.go
@@ -125,7 +125,7 @@ func initCookie() {
 	for i := 0; i < l-1; i++ {
 		if cks[i].Available == True && !CookieOK(&cks[i]) {
 			if pt_key, err := cks[i].OutPool(); err == nil && pt_key != "" {
-				i--
+				i = i - 1
 				logs.Info("正常操作")
 				logs.Info(cks[i].PtPin)
 				logs.Info(i)
