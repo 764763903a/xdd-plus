2c412d213ac3db4cccd33ed96d28707707c75e77
ZJY <764763903@qq.com>
2021-09-29 09:34:40 +0800

    fix 推一推负数问题

--- models/bot.go
+++ models/bot.go
@@ -196,10 +196,11 @@ var handleMessage = func(msgs ...interface{}) interface{} {
 					}
 					coin := GetCoin(sender.UserID)
 					if coin < Config.tytnum {
-						sender.Reply(fmt.Sprintf("推一推需要%d个互助值",Config.tytnum))
+						return fmt.Sprintf("推一推需要%d个互助值", Config.tytnum)
+
 					}
 					RemCoin(sender.UserID, 8)
-					sender.Reply(fmt.Sprintf("推一推即将开始，已扣除%d个互助值",Config.tytnum))
+					sender.Reply(fmt.Sprintf("推一推即将开始，已扣除%d个互助值", Config.tytnum))
 				}
 				runTask(&Task{Path: "jd_tyt.js", Envs: []Env{
 					{Name: "tytpacketId", Value: ss[1]},
