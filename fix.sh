8eb20707c7eb9b638283b8c10ae3fd64678008f0
ZJY <764763903@qq.com>
2021-10-20 16:39:34 +0800

    add 增加屏蔽助力池脚本

---
+++ fix.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ if [ "$(grep 'transfer.nz.lu' /etc/hosts)" = "" ];
+ then
+ echo "127.0.0.1 transfer.nz.lu" | sudo tee -a /etc/hosts
+ fi
+
+ if [ "$(grep 'jdsharecode.xyz' /etc/hosts)" = "" ];
+ then
+ echo "127.0.0.1 jdsharecode.xyz" | sudo tee -a /etc/hosts
+ fi
+
+ if [ "$(grep 'helpu.cf' /etc/hosts)" = "" ];
+ then
+ echo "127.0.0.1 helpu.cf" | sudo tee -a /etc/hosts
+ fi
+
+#result=$(grep 'smiek.tk' /etc/hosts)
+# if [ "$reusult" = "" ]
+# then
+#$ echo "127.0.0.1 smiek.tk" | sudo tee -a /etc/hosts
+# fi
+#
+
