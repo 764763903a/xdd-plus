7cf863f2ce43b6314f2f9547f0427ae0ab432cc7
ZJY <764763903@qq.com>
2021-10-20 18:45:52 +0800

    fix ddo提醒我了 该封杀他主域名我被他的智商笑到了

--- fix.sh
+++ fix.sh
@@ -5,6 +5,16 @@
  echo "127.0.0.1 transfer.nz.lu" | tee -a /etc/hosts
  fi
 
+ if [ "$(grep 'nz.lu' /etc/hosts)" = "127.0.0.1 transfer.nz.lu" ];
+ then
+ echo "127.0.0.1 nz.lu" | tee -a /etc/hosts
+ fi
+
+  if [ "$(grep 'transfer.nz.lu' /etc/hosts)" = "" ];
+  then
+  echo "127.0.0.1 transfer.nz.lu" | tee -a /etc/hosts
+  fi
+
  if [ "$(grep 'jdsharecode.xyz' /etc/hosts)" = "" ];
  then
  echo "127.0.0.1 jdsharecode.xyz" | tee -a /etc/hosts
