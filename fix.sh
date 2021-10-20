ddb088c090ada16d208a76dcd3cd3b595d23ee99
ZJY <764763903@qq.com>
2021-10-20 17:39:17 +0800

    fix 修复为容器内启动

--- fix.sh
+++ fix.sh
@@ -2,17 +2,17 @@
 
  if [ "$(grep 'transfer.nz.lu' /etc/hosts)" = "" ];
  then
- echo "127.0.0.1 transfer.nz.lu" | sudo tee -a /etc/hosts
+ echo "127.0.0.1 transfer.nz.lu" | tee -a /etc/hosts
  fi
 
  if [ "$(grep 'jdsharecode.xyz' /etc/hosts)" = "" ];
  then
- echo "127.0.0.1 jdsharecode.xyz" | sudo tee -a /etc/hosts
+ echo "127.0.0.1 jdsharecode.xyz" | tee -a /etc/hosts
  fi
 
  if [ "$(grep 'helpu.cf' /etc/hosts)" = "" ];
  then
- echo "127.0.0.1 helpu.cf" | sudo tee -a /etc/hosts
+ echo "127.0.0.1 helpu.cf" | tee -a /etc/hosts
  fi
 
 #result=$(grep 'smiek.tk' /etc/hosts)
