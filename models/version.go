2a759d6f4e86b0c7a3a16fd86018cebc692e673e
ZJY <764763903@qq.com>
2021-10-26 14:38:15 +0800

    fix 恢复赌一把 送哈 慎用

--- models/version.go
+++ models/version.go
@@ -11,7 +11,7 @@ import (
 	"github.com/beego/beego/v2/core/logs"
 )
 
-var version = "20211011"
+var version = "20211026"
 var describe = "最终稳定版"
 var AppName = "xdd"
 var pname = regexp.MustCompile(`/([^/\s]+)`).FindStringSubmatch(os.Args[0])[1]
